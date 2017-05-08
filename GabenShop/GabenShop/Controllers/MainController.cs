8ff9f3987691f816a7febd288e032ea0559f9663
ilyak1997 <IlyaK1997@gmail.com>
2017-05-08 12:54:42 +0400

    Del garbage

---
+++ GabenShop/GabenShop/Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GabenShop.Models;
+namespace GabenShop.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        [HttpGet]        
+        public ActionResult Index()
+        {
+            MainModel model = new MainModel();
+            model.Name = "Имя";
+            return View("main", model);
+        }
+    }
+}
---
+++ GabenShop/GabenShop/Models/MainModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GabenShop.Models
+{
+    public class MainModel
+    {
+        public string Name { get; set; }   
+    }
+}
